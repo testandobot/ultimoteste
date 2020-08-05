8b546bb6e3906861ebe406af1bd8525df78481c7
testandobot <68176907+testandobot@users.noreply.github.com>
2020-08-05 00:51:20 -0300

    lulo

--- botreisesse.py
+++ botreisesse.py
@@ -12,17 +12,35 @@ ACCESS_SECRET = os.environ.get('ACCESS_SECRET')
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
 api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
+conta = 0
+contar = 0
+textos = ["\"A esperança vencerá o medo\"\nManda salve pro 3S, melhor presidente de todos os tempos!", "Manda salve pro 3s2, Presidente @LulaOficial", "O MELHOR PRESIDENTE DE TODOS OS TEMPOS\nManda salve pro 3S2, @LulaOficial"]
+print(api.me())
+print(str(len(textos)))
 
-class MyStreamListener(tweepy.StreamListener):
-    def on_status(self, status):
-                if status.user.screen_name == "3s_of" and not "RT" in status.text:
+if __name__ == "__main__":
+    while True:
+        class MyStreamListener(tweepy.StreamListener):
+            def on_status(self, status):
+                global conta
+                global contar
+                if status.user.screen_name == "LulaOficial" and not "RT" in status.text:
                     try:
-                     print(status.text)
-                     api.create_favorite(status.id)
-                     api.retweet(status.id)
+                        if conta > len(textos):
+                            conta = 0
+                        api.update_status(textos[conta], in_reply_to_status_id=status.id, auto_populate_reply_metadata=True)
+                        api.create_favorite(status.id)
+                        api.retweet(status.id)
+                        conta += 1
                     except tweepy.TweepError as e:
-                     print(e.reason)
-
-myStreamListener = MyStreamListener()
-myStream = tweepy.Stream(auth = api.auth, listener=myStreamListener)
-myStream.filter(follow=["1274460778205151232"])
+                        print(e.reason)
+                else:
+                    print(status.text)
+                #   contar += 1
+                #   if contar % 5:
+                #     print("N")
+                  
+            
+        myStreamListener = MyStreamListener()
+        myStream = tweepy.Stream(auth = api.auth, listener=myStreamListener)
+        myStream.filter(follow=["2670726740"])
